5a1e3bce1ed7218e99e889fc9d6663e9436c6fd6
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-24 17:26:07 -0500

    seperated into components

--- components/Button.js
+++ components/Button.js
@@ -9,11 +9,11 @@ const roboto = Roboto({
 })
 
 export default function Button(props) {
-    const { text, dark, full, clickHandler } = props
+    const { text, dark, full, clickHandler } = props;
 
     return (
-        <button onClick={clickHandler} className={'rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 border-solid border-indigo-600 ' + (dark ? ' text-white bg-indigo-600 ' : ' text-indigo-600 ') + (full ? ' grid place-items-center w-full ' : ' ')}>
+        <button onClick={clickHandler} className={'rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 border-solid border-yellow ' + (dark ? ' text-white calmingBlue ' : ' text-yellow-600 ') + (full ? ' grid place-items-center w-full ' : ' ')}>
             <p className={'px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ' + roboto.className}>{text}</p>
         </button>
-    )
+    );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Roboto, Open_Sans} from 'next/font/google'
-import Calendar from "@/components/Calandar";
+import Calendar from "@/components/Calendar";
+import CallToAction from "@/components/CallToAction";
 const roboto = Roboto({
     weight: '400',
     subsets: ['latin'],
@@ -12,7 +13,10 @@ export default function Hero() {
     return (
         <div className='py-4 md:py-10 flex flex-col gap-8 sm:gap-10'>
             <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + roboto.className}><span className='textGradient'>MoodLog</span> helps you track your <span className='textGradient'>daily</span> mood!</h1>
-            <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Create your mood record and see how you feel on <span className='font-semibold'>every day of every year.</span></p>
+            <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>How you feel each day
+                of the year starts here—track your mood and <span
+                    className='font-semibold'> view your daily feelings. </span></p>
+            <CallToAction/>
             <Calendar demo />
         </div>
     )
